a6bcdd1c02345be657ccf308062e7d49394415ad
Jacob Litewski <hackhalotwo@gmail.com>
2015-08-21 05:55:48 -0400

    Pre LD33 commit to start the repo

---
+++ src/com/hackhalo2/ld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hackhalo2.ld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hackhalo2.ld.Dare33;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Dare33(), config);
+	}
+}
